7175846d45c4851a46e9eceaba98b2ef49c9a6d6
janilbols-w <60459708+janilbols-w@users.noreply.github.com>
2024-07-08 15:14:48 +0800

    Update build_and_run.sh

--- nccl-examples/multi-process-test/build_and_run.sh
+++ nccl-examples/multi-process-test/build_and_run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 rm demo
+
+# lib not found issue
+# export LIBRARY_PATH=$LIBRARY_PATH:/PATH/TO/libmpi.so
+# export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/PATH/TO/mpi.h
+
 nvcc -I/usr/local/openmpi/include/ -L/usr/local/openmpi/lib/ -lmpi -lnccl main_nccl.cpp -o demo
 
 echo "mpirun -np $ORION_VGPU --allow-run-as-root demo"
